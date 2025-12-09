3336e64ce6502538586163f021b6834dfe0ec15d
RealBrongField <realbrongfield2010@gmail.com>
2025-12-08 19:07:45 -0800

    Update Character.hx

--- source/Character.hx
+++ source/Character.hx
@@ -859,11 +859,11 @@ class Character extends FlxSprite
 			case 'phone':
 				tex = Paths.getSparrowAtlas('ron/characters/phone');
 				frames = tex;
-				animation.addByPrefix('idle', "Idle", 24);
-				animation.addByPrefix('singUP', 'Sing Up', 24, false);
-				animation.addByPrefix('singDOWN', 'Sing Down', 24, false);
-				animation.addByPrefix('singLEFT', 'Sing Left', 24, false);
-				animation.addByPrefix('singRIGHT', 'Sing Right', 24, false);
+				animation.addByPrefix('idle', "idle", 24);
+				animation.addByPrefix('singUP', 'up', 24, false);
+				animation.addByPrefix('singDOWN', 'down', 24, false);
+				animation.addByPrefix('singLEFT', 'left', 24, false);
+				animation.addByPrefix('singRIGHT', 'right', 24, false);
 				addOffset('idle');
 				addOffset("singUP", 42, 38);
 				addOffset("singLEFT", 98, -27);
